8c9a78f65b60fdc3628fe37c8cc61a588d393e8b
bkilian <kilian.boudeaud@ynov.com>
2026-05-04 09:46:20 +0200

    Character v2.5 mise en comentaire de la partie compshoot

--- Character.js
+++ Character.js
@@ -44,6 +44,6 @@ var player = new Player();
 var enemy1 = new Enemy("Vif-Lame",  100,  100,  10,  20, 0, 0, [], false, 1, 0, 1, 0, false);
 var enemy2 = new Enemy("Arc Calme",     100,  100,  15,  10, 0, 0, [], false, 0, 5, 0, 5, false);
 var enemy3 = new Enemy("Colosse Stable",   250,  250,  15,  5, 0, 0, [], false, 1, 0, 1, 0, false);
-var boss1 = new Boss("Titan Imperçable",  500,  500, _50,  5, 0, 0, [], false, 1, 5, 1, 5, false);
+var boss1 = new Boss("Titan Imperçable",  500,  500, 50,  5, 0, 0, [], false, 1, 5, 1, 5, false);
 var boss2 = new Boss("Maître des Ombres",  300,  300,  50,  20, 0, 0, [], false, 1, 5, 1, 5, false);
 export { player, enemy1, enemy2, enemy3, boss1, boss2 };
